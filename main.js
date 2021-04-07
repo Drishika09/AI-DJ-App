9c25dce5ed059ec48febf1bbb76b2adb26e26333
Drishika09 <74666535+Drishika09@users.noreply.github.com>
2021-04-07 12:24:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song = "";
 canvas="";
+leftwristX = 0;
+leftwristY = 0;
+rightwristX = 0;
+rightwristY = 0;
 function preload() {
     song = loadSound("song1.mp3");
     song = loadSound("song2.mp3");
@@ -10,7 +14,18 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
 }
 function draw(){
 
+}
+function modelLoaded(){
+    console.log("Pose net is initialized");
+}
+function gotPoses(results){
+    if (results.length > 0){
+     console.log(results);
+    }
 }
