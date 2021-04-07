66f8a64f1f07086155a3511dd5b6bc127c773a9b
Drishika09 <74666535+Drishika09@users.noreply.github.com>
2021-04-07 11:43:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 song = "";
 canvas="";
 function preload() {
-    song1 = loadSound("song1.mp3");
-    song2 = loadSound("song2.mp3");
+    song = loadSound("song1.mp3");
+    song = loadSound("song2.mp3");
 }
 function setup(){
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(600,450);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
-
-    image(canvas,0,0,600,400);
 }
 function draw(){
-    
+
 }
